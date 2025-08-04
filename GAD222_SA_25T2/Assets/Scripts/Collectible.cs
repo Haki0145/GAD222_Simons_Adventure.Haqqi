6748cad153cb188bf3d6818684713eedbb73a35b
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2025-08-04 08:27:08 +0400

    Made Collectibles

---
+++ GAD222_SA_25T2/Assets/Scripts/Collectible.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collectible : MonoBehaviour
+{
+    public static int totalCollectibles = 0;
+    public static int collectedCount = 0;
+
+    void Start()
+    {
+        // Count this collectible when spawned
+        totalCollectibles++;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Increment collected count
+            collectedCount++;
+
+            // Destroy this collectible
+            Destroy(gameObject);
+
+            // Check if all collectibles are collected
+            if (collectedCount >= totalCollectibles)
+            {
+                // Load next scene (make sure to add it in Build Settings)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
