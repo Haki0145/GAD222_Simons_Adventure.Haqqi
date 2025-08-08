a3f1dc8350add05d5cdbb82f7cf84af16bd8246a
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2025-08-08 18:38:35 +0400

    Added spawn point

--- GAD222_SA_25T2/Assets/Scripts/PlayerHealth.cs
+++ GAD222_SA_25T2/Assets/Scripts/PlayerHealth.cs
@@ -5,6 +5,12 @@ public class PlayerHealth : MonoBehaviour
 {
     public int PlayerHealthHealth = 100;
     public Slider Playerhealthbar;
+    private Vector3 spawnPoint;
+
+    private void Start()
+    {
+        spawnPoint = transform.position;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,13 +22,25 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage()
     {
-        PlayerHealthHealth += -33;
+        PlayerHealthHealth -= 33;
+
+        if (PlayerHealthHealth <= 0)
+        {
+            PlayerHealthHealth = 0;
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        PlayerHealthHealth = 100;
+        transform.position = spawnPoint;
+        Debug.Log("Player respawned!");
     }
 
     private void Update()
     {
         Debug.Log(PlayerHealthHealth);
-
         Playerhealthbar.value = PlayerHealthHealth;
     }
 }
